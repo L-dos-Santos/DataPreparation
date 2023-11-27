c3b49ec044cc68361730e32615688fe6576c040b
L-dos-Santos <2020311@student.cct.ie>
2023-11-27 17:03:11 +0000

    Checking the missing values

--- dataprep.R
+++ dataprep.R
@@ -2,6 +2,17 @@
 vaccination <- read.csv("country_vaccinations.csv", stringsAsFactors = )
 head(vaccination)
 
+#checking missing values
+which(is.na(vaccination$country))
+which(is.na(vaccination$iso_code))
+which(is.na(vaccination$date))
+which(is.na(vaccination$total_vaccinations))
+which(is.na(vaccination$people_vaccinated))
+which(is.na(vaccination$people_fully_vaccinated))
+
+
 #deleting the missing values
 df_vaccination <- na.omit(vaccination)
 head(df_vaccination)
+
+
