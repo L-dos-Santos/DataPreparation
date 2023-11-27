8e2268790ee2728695f6c4e798783d978bcfdae9
L-dos-Santos <2020311@student.cct.ie>
2023-11-27 16:17:51 +0000

    Handling missing values

--- dataprep.R
+++ dataprep.R
@@ -1,2 +1,7 @@
+#reading the dataset
 vaccination <- read.csv("country_vaccinations.csv", stringsAsFactors = )
+head(vaccination)
 
+#deleting the missing values
+df_vaccination <- na.omit(vaccination)
+head(df_vaccination)
